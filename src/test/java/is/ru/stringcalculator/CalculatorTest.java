15cd07b62b78ec2750c0602e5cc246dd727dbbd9
maggawaage <maggawaage@gmail.com>
2017-10-11 13:29:46 +0000

    New lines between numbers 1\n2,3 return 6

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -24,4 +24,9 @@ public class CalculatorTest{
     public void testMultiplesNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+
+    @Test //5
+    public void testNewLinesBetweenNumbers(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
  }
