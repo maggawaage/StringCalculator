f4387557e5937b668a2495eb682891c4c625c20c
maggawaage <maggawaage@gmail.com>
2017-10-11 12:15:56 +0000

    Test One Number

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 
 public class CalculatorTest{
 
-	@Test
+	@Test //1
     public void testEmptyString() {
         assertEquals(0, Calculator.add(""));
     }
-}
+
+    @Test
+    public void testOneNumber() {
+    	assertEquals(1, Calculator.add("1"));
+    }
+ }
